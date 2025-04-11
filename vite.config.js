3f20daa5d75b98abb6c93442fba44eba67c2b8cc
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-04-11 22:20:49 +0200

    ci

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
-  base: process.env === "production" ? "/anzi-project/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/anzi-project/" : "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
